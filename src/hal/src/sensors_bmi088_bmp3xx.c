c2d4aa64ba5458a000190e0b31ecbee58983eb03
darren-chiu <darrenchiu321@ghmail.com>
2025-09-11 14:09:29 -0700

    modified files

--- src/hal/src/sensors_bmi088_bmp3xx.c
+++ src/hal/src/sensors_bmi088_bmp3xx.c
@@ -324,7 +324,7 @@ static void sensorsTask(void *param)
       sensorsAlignToAirframe(&gyroScaledIMU, &sensorData.gyro);
 
       // Do not apply low pass, the delay messes with the controller.
-      // applyAxis3fLpf((lpf2pData*)(&gyroLpf), &sensorData.gyro);
+      applyAxis3fLpf((lpf2pData*)(&gyroLpf), &sensorData.gyro);
 
       measurement.type = MeasurementTypeGyroscope;
       measurement.data.gyroscope.gyro = sensorData.gyro;
@@ -336,8 +336,9 @@ static void sensorsTask(void *param)
       accScaledIMU.z = accelRaw.z * SENSORS_BMI088_G_PER_LSB_CFG / accScale;
       sensorsAlignToAirframe(&accScaledIMU, &accScaled);
       sensorsAccAlignToGravity(&accScaled, &sensorData.acc);
+
       // Do not apply low pass, the delay messes with the controller.
-      // applyAxis3fLpf((lpf2pData*)(&accLpf), &sensorData.acc);
+      applyAxis3fLpf((lpf2pData*)(&accLpf), &sensorData.acc);
 
       measurement.type = MeasurementTypeAcceleration;
       measurement.data.acceleration.acc = sensorData.acc;
